b80a4ffc44b1696f24ddde2d70c0ca5f5e2db026
George Chang <changg@gmail.com>
2019-03-09 20:21:17 -0500

    Initial commit

---
+++ src/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ScHost
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+    }
+}
